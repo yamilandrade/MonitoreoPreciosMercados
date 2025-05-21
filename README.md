8be958d0eb98c5e6ff98ecaa043be009d1f0f492
yamilandrade <yamil.andrade@gmail.com>
2025-05-21 15:35:38 -0400

    first Proyecto Inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+# angularadmindashfree
+Angular 19: Build a Full-Stack Dashboard <br>
+[Live Demo
+](https://therichpost.com/angular-19-build-a-full-stack-dashboard/)
+
+<br>
+Guys please run below commands to run the complete project<br>
+<br>
+
+**npm install**
+
+<br>
+
+**ng serve**
---
+++ src/app/demo/pages/authentication/auth-signin/auth-signin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-auth-signin',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './auth-signin.component.html',
+  styleUrls: ['./auth-signin.component.scss']
+})
+export default class AuthSigninComponent {}
